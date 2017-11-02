3752df191b6efb298e2cc2e4ff0614c268475a93
Danieldbc <Danieldbc@github.com>
2017-11-02 08:28:45 +0100

    Primer apartat

---
+++ app/src/main/java/edu/eseiaat/upc/pma/borreguero/daniel/danielborreguero/DanielBorregueroMainActivity.java
@@ -0,0 +1,13 @@
+package edu.eseiaat.upc.pma.borreguero.daniel.danielborreguero;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DanielBorregueroMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daniel_borreguero_main);
+    }
+}
